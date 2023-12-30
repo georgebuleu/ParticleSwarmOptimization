d6b1b4020e0a40bdab971448ae24eba05c7eeb80
georgebuleu <georgebu44@gmail.com>
2023-12-30 18:19:25 +0200

    moved pso files to new project

--- SwarmOptimizationOLD/Program.cs
+++ SwarmOptimizationOLD/Program.cs
@@ -5,19 +5,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        var problem = new Problem(
-            x => Math.Pow(x[0], 2) + Math.Pow(x[1], 2),
-            new (double Min, double Max)[] { (-10, 10), (-10, 10) }
-        );
-        var alpha = 0.1;
-        //set parameters
-        Parameters.MaxVelocity = alpha * (problem.Domain[0].Max - problem.Domain[0].Min);
-        var pso = new Pso(problem);
-        var p = pso.OptimizeGbest();
-        Console.WriteLine($"Best solution found: {p.Cost}");
-        Console.WriteLine($"Best solution found at: {string.Join(", ", p.Position)}");
-         p = pso.OptimizeLbest();
-        Console.WriteLine($"Best solution found: {p.Cost}");
-        Console.WriteLine($"Best solution found at: {string.Join(", ", p.Position)}");
+        
     }
 }
